02c30bee46ab74825bb000430e916d3039d3bddc
Anvay Vahia <110357337+CyberLord09@users.noreply.github.com>
2025-05-31 20:31:06 -0700

    Update config.js

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -4,7 +4,7 @@ if (location.hostname === "localhost") {
 } else if (location.hostname === "127.0.0.1") {
   pythonURI = "http://127.0.0.1:3167";
 } else {
-  pythonURI = "https://crosswise.stu.nighthawkcodingsociety.com";
+  pythonURI = "https://crosswise.opencodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
